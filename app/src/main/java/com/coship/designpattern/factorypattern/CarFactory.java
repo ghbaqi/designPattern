6235d8508502cb2c621e969def025db26cc703ed
e <ghbaqi@gmail.com>
2017-08-09 23:03:29 +0800

    构建者模式，工厂方法模式，原型模式，策略模式

---
+++ app/src/main/java/com/coship/designpattern/factorypattern/CarFactory.java
@@ -0,0 +1,8 @@
+package com.coship.designpattern.factorypattern;
+
+/**
+ * Created by ghbaqi on 2017/8/9.
+ */
+
+public class CarFactory {
+}
---
+++ app/src/main/java/com/coship/designpattern/yuanxing/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.coship.designpattern.yuanxing;
+
+/**
+ * Created by ghbaqi on 2017/8/8.
+ */
+
+class LoginActivity {
+}
