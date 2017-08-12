3ef2a6475408e6cd040ea5fbca1fe5ea5c7bdeb1
e <ghbaqi@gmail.com>
2017-08-12 18:43:18 +0800

    责任链模式，踩坑2个

---
+++ app/src/main/java/com/coship/designpattern/iteratorpattern/IteratorActivity.java
@@ -0,0 +1,8 @@
+package com.coship.designpattern.iteratorpattern;
+
+/**
+ * Created by ghbaqi on 2017/8/12.
+ */
+
+public class IteratorActivity {
+}
---
+++ app/src/main/java/com/coship/designpattern/zhuangtai/PowerOffControl.java
@@ -0,0 +1,8 @@
+package com.coship.designpattern.zhuangtai;
+
+/**
+ * Created by ghbaqi on 2017/8/10.
+ */
+
+public class PowerOffControl {
+}
